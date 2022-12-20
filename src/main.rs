f229117e25949bf28e6cda419becdb5ef6c64e7a
1noro <ppuubblliicc@protonmail.com>
2022-12-20 17:52:00 +0100

    feature: healthcheck variable
    dependiendo del header accept

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 #[macro_use] extern crate rocket;
 
-#[get("/healthcheck")]
-fn health_check() -> &'static str {
-    "OK\n"
+use rocket::serde::{Serialize, json::Json};
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Status {
+    status: &'static str
+}
+
+#[get("/healthcheck", format = "application/json")]
+fn health_check_json() -> Json<Status> {
+    Json(Status { status: "OK" })
+}
+
+#[get("/healthcheck", format = "text/plain", rank = 2)]
+fn health_check_plain() -> &'static str {
+    "OK"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/v1", routes![health_check])
+    rocket::build().mount("/v1", routes![health_check_json, health_check_plain])
 }
